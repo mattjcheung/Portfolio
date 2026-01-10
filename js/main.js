1e427e27c8a5d8dffbdc06c42c16e619d302513f
mattjcheung <mattjcheung03@gmail.com>
2026-01-10 13:51:23 +1100

    very basic html css version

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// main.js
+
+// Example: simple console message
+console.log("Portfolio loaded successfully");
+
+// Later you can add:
+// - smooth scrolling
+// - theme toggle (dark mode)
+// - project filtering
